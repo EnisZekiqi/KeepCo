92e3318a4fe7f2240dffda37173791337e4e1c38
eniszekiqi <enis_zekiqi@hotmail.com>
2024-03-10 23:18:31 +0100

    finished project

--- src/features/Career.js
+++ src/features/Career.js
@@ -7,8 +7,8 @@ import Box from '@mui/material/Box';
 const Career = () => {
     const[isClicked,setIsClicked]=useState(false)
 
-    const showClick = async (contentId)=>{
-    setIsClicked(contentId)
+    const showClick =  (position)=>{
+      setIsClicked(isClicked === position ? '' : position);
     }
 
     
@@ -28,56 +28,78 @@ const Career = () => {
         phone: '',
         email: '',
       });
-      const [lastApplication, setLastApplication] = useState(null);
+      const [lastApplications, setLastApplications] = useState({
+        marketingExpert: null,
+        frontEndDeveloper: null,
+        serviceContact: null,
+      });
+      const [showLastApplications, setShowLastApplications] = useState({
+        marketingExpert: false,
+        frontEndDeveloper: false,
+        serviceContact: false,
+      });
     
       const handleChange = (e) => {
         const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
+        setFormData((prevData) => ({
+          ...prevData,
+          [name]: value,
+        }));
       };
     
-      const handleSubmit = () => {
-        setLastApplication(formData);
-        // You can perform any additional actions here, like sending the data to a server
+      const handleSubmit = (e, position) => {
+        e.preventDefault();
+        // Save form data to the respective lastApplications state based on the position
+        setLastApplications((prevApplications) => ({
+          ...prevApplications,
+          [position]: formData,
+        }));
+        // Show last submitted application for the respective position
+        setShowLastApplications((prevShow) => ({
+          ...prevShow,
+          [position]: true,
+        }));
+        // Clear form data
+        setFormData({
+          firstname: '',
+          lastname: '',
+          age: '',
+          country: '',
+          phone: '',
+          email: '',
+        });
       };
-
-      const [showLastApplication,setShowLastApplication]=useState(false)
-      useEffect(() => {
-        if (lastApplication) {
-          const timer = setTimeout(() => {
-            setShowLastApplication(true);
-          }, 2000); // 2 seconds delay
-          return () => clearTimeout(timer);
-        }
-      }, [lastApplication]);
     
-      const handleHideLastApplication = () => {
-        setShowLastApplication(false);
+      const handleHideLastApplication = (position) => {
+        // Hide last submitted application for the respective position
+        setShowLastApplications((prevShow) => ({
+          ...prevShow,
+          [position]: false,
+        }));
       };
-    
-  
 
     return ( 
 
     
         <div>
             <h1>Career</h1>
-            <div style={{marginTop:50}} className="row">
-                <div style={{width:'fit-content'}} className="d-flex flex-column col">
+            <div style={{marginTop:50 ,gap:50}} className="d-flex flex-lg-row flex-column">
+                <div style={{width:'fit-content'}} className="d-flex flex-column ">
                     <h1>SKILLED AND <b className="bold">DEDICATED</b> EFFICIEND AND <b className="bold">FOCUSED</b></h1>
                 </div>
-                <div style={{fontSize:18}} className="col">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto doloribus accusamus sit nisi eius asperiores debitis et error autem facilis!
-                    Architecto doloribus accusamus sit nisi eius asperiores debitis et error autem facilisus sit nisi eius asperiores debitis et error au
+                <div style={{fontSize:18}} className="col-md-6">
+                    <p>
+                    We partner up with our clients by setting up, managing and operating their extended teams across Software Development, Quality Assurance, Customer Support, Technical Support and Business process outsourcing services. We make sure that our teams remain satisfied and therefore dedicated to our client’s needs. This makes us reliable, effective and productive.
                     </p>
                 </div>
             </div>
             <h1 style={{marginTop:100}} className="text-center">Available Positions</h1>
             <div style={{marginTop:50}} className="d-flex flex-column">
-                <div style={{borderBottom:'1px solid #bbbbbb'}} className="positon1 d-flex justify-content-between">
-                    <button id="position1" onClick={()=>showClick('position1')} style={{backgroundColor:'transparent',border:'none'}}>
+                <div style={{borderBottom:'1px solid #bbbbbb'}} className="positon1 d-flex flex-lg-row flex-column justify-content-lg-between justify-content-center">
+                    <button id="position1" onClick={()=>showClick('marketingExpert')} style={{backgroundColor:'transparent',border:'none'}}>
                     <div className="d-flex flex-column">
                     <h2 style={{color:'#ffffff'}}>Marketing Expert</h2>
-                    <p className="text-start" style={{color:'#bbbbbb',fontSize:14}}>
+                    <p className="text-lg-start text-center" style={{color:'#bbbbbb',fontSize:14}}>
                         18/12/2023
                     </p>
                     </div>
@@ -88,8 +110,8 @@ const Career = () => {
                      transition={{
                       duration:1,
                     }}
-                    className={`showedposition1 ${isClicked === 'position1' ? 'showedposition1x' : ''} ${isClicked === 'removeposition1' ? 'showedposition1' : ''}`}>
-                       <form>
+                    className={`showedposition1 ${isClicked === 'marketingExpert' ? 'showedposition1x' : ''} ${isClicked === 'removemarketingExpert' ? 'showedposition1' : ''}`}>
+                       <form onSubmit={(e) => handleSubmit(e, 'marketingExpert')}>
                        <div style={{gap:20 ,marginLeft:-30,marginTop:30}} className="d-flex" >
                         <div className="d-flex flex-column">
                         <label htmlFor="firstname">First Name:</label><br />
@@ -97,44 +119,44 @@ const Career = () => {
                         </div>
                         <div className="d-flex flex-column">
                           <label htmlFor="lastname">Last Name:</label><br />
-                         <input style={{color:'white',marginTop:-10}} className="llabellat" type="text" id="lastname" name="lastname" value={formData.lastname} onChange={handleChange} /><br />
+                         <input style={{color:'white',marginTop:-10,backgroundColor:'#101419'}} className="llabellat" type="text" id="lastname" name="lastname" value={formData.lastname} onChange={handleChange} /><br />
                         </div>  
                         </div>
                     <div style={{gap:20 ,marginLeft:-30}} className="d-flex">
                     <div className="d-flex flex-column">
                     <label htmlFor="age">Age:</label><br />
-                    <input style={{color:'white',marginTop:-10}} className="llabellat" type="number" id="age" name="age" value={formData.age} onChange={handleChange} /><br />
+                    <input style={{color:'white',marginTop:-10,backgroundColor:'#101419'}} className="llabellat" type="number" id="age" name="age" value={formData.age} onChange={handleChange} /><br />
                     </div>
                     <div className="d-flex flex-column">
                     <label htmlFor="country">Country:</label><br />
-                    <input style={{color:'white',marginTop:-10}} className="llabellat" type="text" id="country" name="country" value={formData.country} onChange={handleChange} /><br />
+                    <input style={{color:'white',marginTop:-10,backgroundColor:'#101419'}} className="llabellat" type="text" id="country" name="country" value={formData.country} onChange={handleChange} /><br />
                     </div>
                     </div>
                     <div style={{gap:20,marginLeft:-30}}  className="d-flex">
                     <div className="d-flex flex-column">
                     <label htmlFor="phone">Phone:</label><br />
-                    <input style={{color:'white',marginTop:-10}} className="llabellat" type="text" id="phone" name="phone" value={formData.phone} onChange={handleChange} /><br />
+                    <input style={{color:'white',marginTop:-10,backgroundColor:'#101419'}} className="llabellat" type="text" id="phone" name="phone" value={formData.phone} onChange={handleChange} /><br />
                     </div>
                     <div className="d-flex flex-column">
                     <label htmlFor="email">Email:</label><br />
-                    <input style={{color:'white',marginTop:-10}} className="llabellat" type="email" id="email" name="email" value={formData.email} onChange={handleChange} /><br /><br />
+                    <input style={{color:'white',marginTop:-10,backgroundColor:'#101419'}} className="llabellat" type="email" id="email" name="email" value={formData.email} onChange={handleChange} /><br /><br />
                     </div>
                     </div>
                     <div className="upload-cs">
                       <h4>Upload CV</h4>
                     </div>
                     <div className="d-flex justify-content-evenly">
-                    <Button style={{marginTop:20}} id="removepostion1" onClick={()=>showClick('removepositon1')} className="xcx" variant="outlined">Show Less</Button>
-                    <Button style={{marginTop:20}} id="removepostion1"  onClick={handleSubmit} className="xcx" variant="outlined">Apply Now</Button>
+                    <Button style={{marginTop:20}} id="removepostion1" onClick={()=>showClick('removemarketingExpert')} className="xcx" variant="outlined">Show Less</Button>
+                    <Button style={{marginTop:20}} id="removepostion1"   onClick={(e) => handleSubmit(e, 'marketingExpert')} className="xcx" variant="outlined">Apply Now</Button>
                     </div>
                        </form>
                     </motion.div>
                 </div>
-                <div style={{borderBottom:'1px solid #bbbbbb',marginTop:20}} className="positon1 d-flex justify-content-between">
-                    <button id="position2" onClick={()=>showClick('position2')} style={{backgroundColor:'transparent',border:'none'}}>
+                <div style={{borderBottom:'1px solid #bbbbbb',marginTop:20}} className="positon1 d-flex flex-lg-row flex-column justify-content-between">
+                    <button id="position2" onClick={() => showClick('frontEndDeveloper')}  style={{backgroundColor:'transparent',border:'none'}}>
                     <div className="d-flex flex-column">
                     <h2 style={{color:'#ffffff'}}>Front-End Developer</h2>
-                    <p className="text-start" style={{color:'#bbbbbb',fontSize:14}}>
+                    <p className="text-lg-start text-center" style={{color:'#bbbbbb',fontSize:14}}>
                         23/11/2023
                     </p>
                     </div>
@@ -145,8 +167,8 @@ const Career = () => {
                      transition={{
                       duration:1,
                     }}
-                    className={`showedposition1 ${isClicked === 'position2' ? 'showedposition1x' : ''} ${isClicked === 'removeposition2' ? 'showedposition1' : ''}`}>
-                    <form>
+                    className={`showedposition1 ${isClicked === 'frontEndDeveloper' ? 'showedposition1x' : ''} ${isClicked === 'removefrontEndDeveloper' ? 'showedposition1' : ''}`}>
+                    <form  onSubmit={(e) => handleSubmit(e, 'frontEndDeveloper')}>
                        <div style={{gap:20 ,marginLeft:-30,marginTop:30}} className="d-flex" >
                         <div className="d-flex flex-column">
                         <label htmlFor="firstname">First Name:</label><br />
@@ -182,16 +204,16 @@ const Career = () => {
                       <h4>Upload CV</h4>
                     </div> 
                     <div className="d-flex justify-content-evenly">
-                    <Button style={{marginTop:20}} id="removepostion2" onClick={()=>showClick('removepositon2')} className="xcx" variant="outlined">Show Less</Button>
-                    <Button style={{marginTop:20}} id="removepostion2" onClick={()=>showClick('removepositon2')} className="xcx" variant="outlined">Apply Now</Button>
+                    <Button style={{marginTop:20}} id="removepostion2" onClick={() => showClick('removefrontEndDeveloper')} className="xcx" variant="outlined">Show Less</Button>
+                    <Button style={{marginTop:20}} id="removepostion2" onClick={(e) => handleSubmit(e, 'frontEndDeveloper')} className="xcx" variant="outlined">Apply Now</Button>
                     </div>
                     </motion.div>
                 </div>
-                <div style={{borderBottom:'1px solid #bbbbbb',marginTop:20}} className="positon1 d-flex justify-content-between">
-                    <button id="position3" onClick={()=>showClick('position3')} style={{backgroundColor:'transparent',border:'none'}}>
+                <div style={{borderBottom:'1px solid #bbbbbb',marginTop:20}} className="positon1 d-flex flex-lg-row flex-column justify-content-between">
+                    <button id="position3"  onClick={() => showClick('serviceContact')} style={{backgroundColor:'transparent',border:'none'}}>
                     <div className="d-flex flex-column">
                     <h2 style={{color:'#ffffff'}}>Service Contact (2x)</h2>
-                    <p className="text-start" style={{color:'#bbbbbb',fontSize:14}}>
+                    <p className="text-lg-start text-center" style={{color:'#bbbbbb',fontSize:14}}>
                         27/11/2023
                     </p>
                     </div>
@@ -202,8 +224,8 @@ const Career = () => {
                      transition={{
                       duration:1,
                     }}
-                    className={`showedposition1 ${isClicked === 'position3' ? 'showedposition1x' : ''} ${isClicked === 'removeposition3' ? 'showedposition1' : ''}`}>
-                    <form>
+                    className={`showedposition1 ${isClicked === 'serviceContact' ? 'showedposition1x' : ''} ${isClicked === 'removeserviceContact' ? 'showedposition1' : ''}`}>
+                    <form onSubmit={(e) => handleSubmit(e, 'serviceContact')}>
                        <div style={{gap:20 ,marginLeft:-30,marginTop:30}} className="d-flex" >
                         <div className="d-flex flex-column">
                         <label htmlFor="firstname">First Name:</label><br />
@@ -239,23 +261,53 @@ const Career = () => {
                       <h4>Upload CV</h4>
                     </div> 
                     <div className="d-flex justify-content-evenly">
-                    <Button style={{marginTop:20}} id="removepostion3" onClick={()=>showClick('removepositon3')} className="xcx" variant="outlined">Show Less</Button>
-                    <Button style={{marginTop:20}} id="removepostion3" onClick={()=>showClick('removepositon3')} className="xcx" variant="outlined">Apply Now</Button>
+                    <Button style={{marginTop:20}} id="removepostion3"onClick={() => showClick('removeserviceContact')} className="xcx" variant="outlined">Show Less</Button>
+                    <Button style={{marginTop:20}} id="removepostion3"onClick={(e) => handleSubmit(e, 'serviceContact')} className="xcx" variant="outlined">Apply Now</Button>
                     </div>
                     </motion.div>
                 </div>
-                {lastApplication && showLastApplication &&(
-        <div style={{marginTop:50}} className="d-flex flex-column justfiy-content-center align-items-center">
-          <h2>Last Submitted Application:</h2>
-          <p>First Name: {lastApplication.firstname}</p>
-          <p>Last Name: {lastApplication.lastname}</p>
-          <p>Age: {lastApplication.age}</p>
-          <p>Country: {lastApplication.country}</p>
-          <p>Phone: {lastApplication.phone}</p>
-          <p>Email: {lastApplication.email}</p>
-          <Button className="xcx" variant="outlined" onClick={handleHideLastApplication}>Show Less</Button>
-        </div>
-      )}
+                {lastApplications.marketingExpert && showLastApplications.marketingExpert && (
+                <div style={{ marginTop: 50 }} className="d-flex flex-column justfiy-content-center align-items-center">
+                  <h2> Submitted <b className="bold">Marketing Expert</b> Application:</h2>
+                  <p>First Name: <b className="bold">{lastApplications.marketingExpert.firstname}</b></p>
+                  <p>Last Name:  <b className="bold">{lastApplications.marketingExpert.lastname}</b></p>
+                  <p>Age:  <b className="bold">{lastApplications.marketingExpert.age}</b></p>
+                  <p>Country: <b className="bold">{lastApplications.marketingExpert.country}</b></p>
+                  <p>Phone: <b className="bold">{lastApplications.marketingExpert.phone}</b></p>
+                  <p>Email: <b className="bold">{lastApplications.marketingExpert.email}</b></p>
+                  <Button className="xcx" variant="outlined" onClick={() => handleHideLastApplication('marketingExpert')}>
+                    Show Less
+                  </Button>
+                </div>
+              )}
+                {lastApplications.frontEndDeveloper && showLastApplications.frontEndDeveloper && (
+                <div style={{ marginTop: 50 }} className="d-flex flex-column justfiy-content-center align-items-center">
+                  <h2> Submitted <b className="bold">Front-End Developer</b> Application:</h2>
+                  <p>First Name: <b className="bold">{lastApplications.frontEndDeveloper.firstname}</b></p>
+                  <p>Last Name: <b className="bold">{lastApplications.frontEndDeveloper.lastname}</b></p>
+                  <p>Age: <b className="bold">{lastApplications.frontEndDeveloper.age}</b></p>
+                  <p>Country: <b className="bold">{lastApplications.frontEndDeveloper.country}</b></p>
+                  <p>Phone: <b className="bold">{lastApplications.frontEndDeveloper.phone}</b></p>
+                  <p>Email: <b className="bold">{lastApplications.frontEndDeveloper.email}</b></p>
+                  <Button className="xcx" variant="outlined" onClick={() => handleHideLastApplication('frontEndDeveloper')}>
+                    Show Less
+                  </Button>
+                </div>
+              )}
+              {lastApplications.serviceContact && showLastApplications.serviceContact && (
+                <div style={{ marginTop: 50 }} className="d-flex flex-column justfiy-content-center align-items-center">
+                  <h2> Submitted <b className="bold">Service Contact (2x)</b> Application:</h2>
+                  <p>First Name: <b className="bold">{lastApplications.serviceContact.firstname}</b></p>
+                  <p>Last Name:  <b className="bold">{lastApplications.serviceContact.lastname}</b></p>
+                  <p>Age:  <b className="bold">{lastApplications.serviceContact.age}</b></p>
+                  <p>Country: <b className="bold">{lastApplications.serviceContact.country}</b></p>
+                  <p>Phone: <b className="bold">{lastApplications.serviceContact.phone}</b></p>
+                  <p>Email: <b className="bold">{lastApplications.serviceContact.email}</b></p>
+                  <Button className="xcx" variant="outlined" onClick={() => handleHideLastApplication('serviceContact')}>
+                    Show Less
+                  </Button>
+                </div>
+              )}
             </div>
             <div className="empty4"></div>
         </div>
--- src/features/Transfer.js
+++ src/features/Transfer.js
@@ -3,40 +3,6 @@ import React, { useState ,useEffect} from 'react';
 
 const Transfer = () => {
 
-    const [formData, setFormData] = useState({
-        firstname: '',
-        lastname: '',
-        age: '',
-        country: '',
-        phone: '',
-        email: '',
-      });
-      const [lastApplication, setLastApplication] = useState(null);
-    
-      const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-      };
-    
-      const handleSubmit = () => {
-        setLastApplication(formData);
-        // You can perform any additional actions here, like sending the data to a server
-      };
-
-      const [showLastApplication,setShowLastApplication]=useState(false)
-      useEffect(() => {
-        if (lastApplication) {
-          const timer = setTimeout(() => {
-            setShowLastApplication(true);
-          }, 2000); // 2 seconds delay
-          return () => clearTimeout(timer);
-        }
-      }, [lastApplication]);
-    
-      const handleHideLastApplication = () => {
-        setShowLastApplication(false);
-      };
-    
     
     return ( 
         <div>
@@ -53,33 +19,6 @@ const Transfer = () => {
            <li style={{color:'#3399ff',fontSize:17}}> Chosen</li>
            <p className='d-flex justify-content-start' style={{color:'#bbbbbb'}}>In the chosen list are the features that you can have choosen to have in the editor for the files </p>
            </div>
-           <form>
-            <label htmlFor="firstname">First Name:</label><br />
-            <input type="text" id="firstname" name="firstname" value={formData.firstname} onChange={handleChange} /><br />
-            <label htmlFor="lastname">Last Name:</label><br />
-            <input type="text" id="lastname" name="lastname" value={formData.lastname} onChange={handleChange} /><br />
-            <label htmlFor="age">Age:</label><br />
-            <input type="number" id="age" name="age" value={formData.age} onChange={handleChange} /><br />
-            <label htmlFor="country">Country:</label><br />
-            <input type="text" id="country" name="country" value={formData.country} onChange={handleChange} /><br />
-            <label htmlFor="phone">Phone:</label><br />
-            <input type="text" id="phone" name="phone" value={formData.phone} onChange={handleChange} /><br />
-            <label htmlFor="email">Email:</label><br />
-            <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} /><br /><br />
-            <button type="button" onClick={handleSubmit}>Submit</button>
-            </form>
-            {lastApplication && showLastApplication &&(
-        <div>
-          <h2>Last Submitted Application:</h2>
-          <p>First Name: {lastApplication.firstname}</p>
-          <p>Last Name: {lastApplication.lastname}</p>
-          <p>Age: {lastApplication.age}</p>
-          <p>Country: {lastApplication.country}</p>
-          <p>Phone: {lastApplication.phone}</p>
-          <p>Email: {lastApplication.email}</p>
-          <button onClick={handleHideLastApplication}>ShowLess</button>
-        </div>
-      )}
             <div className="empty4"></div>
         </div>
      );
